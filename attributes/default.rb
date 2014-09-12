7bdbf8365ceb882868185361b5e3248e335be203
Peter Mooshammer <pmoosh@yahoo.com>
2014-09-12 08:28:52 -0700

    added os based security

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,4 @@
-normal['jenkins']['master']['jenkins_args'] = '--argumentsRealm.passwd.admin=admin --argumentsRealm.roles.admin=admin'
+normal['jenkins']['master']['install_method'] = 'war'
+normal['jenkins']['master']['version'] = '1.555'
+normal['jenkins']['master']['source'] = "#{node['jenkins']['master']['mirror']}/war/#{node['jenkins']['master']['version']}/jenkins.war"
+default['eas-jenkins']['AuthorizationStrategy'] = 'FullControlOnceLoggedInAuthorizationStrategy'
--- recipes/_auth_os.rb
+++ recipes/_auth_os.rb
@@ -1,4 +1,4 @@
-#
+
 # Cookbook Name:: eas-jenkins
 # Recipe:: default
 #
@@ -18,45 +18,6 @@
 #
 
 
-include_recipe 'jenkins::java'
-include_recipe 'jenkins::master'
-include_recipe 'git'
-
-user 'chef'
-
-require 'net/ssh'
-
-key = OpenSSL::PKey::RSA.new(4096)
-private_key = key.to_pem
-public_key  = "#{key.ssh_type} #{[key.to_blob].pack('m0')}"
-
-# Create the Jenkins user with the public key
-jenkins_user 'chef' do
-  public_keys [public_key]
-end
-
-
-# Set the private key on the Jenkins executor
-ruby_block 'set private key' do
-  block { node.run_state[:jenkins_private_key] = private_key }
-end
-
-%w(github gerrit).each do |plugin|
-  jenkins_plugin plugin do
-    notifies :restart, 'service[jenkins]'
-  end
-end
-
-group 'shadow' do
-  action :modify
-  members [node['jenkins']['master']['user']]
-  append true
-end
-
-include_recipe 'eas-jenkins::_auth_os'
-
-jenkins_command 'login'
-
 jenkins_script 'setup authentication' do
   command <<-EOH.gsub(/^ {4}/, '')
     import jenkins.model.*
@@ -71,17 +32,5 @@ jenkins_script 'setup authentication' do
 
     instance.save()
   EOH
-end
-
-%w(test_eas test2_eas).each do |job|
-  xml = File.join(Chef::Config[:file_cache_path], "#{job}-config.xml")
-
-  cookbook_file xml do
-    source "#{job}-config.xml"
-  end
-
-  jenkins_job job do
-    config xml
-  end
 end
 
