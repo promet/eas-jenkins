999e23359ec41d348b0ee61b5372ee9d8de7a517
Peter Mooshammer <pmoosh@yahoo.com>
2014-09-30 23:40:49 -0700

    incr version number

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@opscale.com'
 license 'Apache 2.0'
 description 'Installs/Configures eas-jenkins'
 long_description 'Installs/Configures eas-jenkins'
-version '0.1.1'
+version '0.1.2'
 
 depends 'jenkins', '~> 2.1.2'
 depends 'git', '~> 4.0.2'
